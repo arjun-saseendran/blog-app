8da058cd9e66d5b67dfbc0e6aa05d5d5a0356be7
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-18 17:42:57 +0530

    create logout

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -50,15 +50,17 @@ export class AuthService{
         return null;
 
     }
-    async logout(){}
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+            
+        } catch (error) {
+            console.log("Appwrite service :: logout() :: ",error);
+        }
+    }
 
 
 }
 
-
-
- const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('<PROJECT_ID>');                 // Your project ID
-
-const account = new Account(client);
+const authService = new AuthService();
+export default authService;
