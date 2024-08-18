e0f1ae63d15aa9bda138e8c6a3f8340072742f61
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-18 18:06:18 +0530

    add DBService

--- src/appwrite/db.js
+++ src/appwrite/db.js
@@ -1,1 +1,17 @@
-import config from '..config/config'
+import config from '../config/config';
+import { Client, Databases, Storage, Query } from 'appwrite';
+
+export class DBService{
+    client = new Client()
+    databases;
+    bucket;
+
+    constructor(){
+        this.client.setEndpoint(config.appwriteUrl)
+        .setProject(config.appwriteProjectId)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+
+    }
+
+}
