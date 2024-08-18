8b0334caaaa0b525ef4ac723d3b230492bde1b29
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-18 18:17:20 +0530

    create getPost

--- src/appwrite/db.js
+++ src/appwrite/db.js
@@ -1,17 +1,29 @@
-import config from '../config/config';
-import { Client, Databases, Storage, Query } from 'appwrite';
+import config from "../config/config";
+import { Client, Databases, Storage, Query } from "appwrite";
 
-export class DBService{
-    client = new Client()
-    databases;
-    bucket;
+export class DBService {
+  client = new Client();
+  databases;
+  bucket;
 
-    constructor(){
-        this.client.setEndpoint(config.appwriteUrl)
-        .setProject(config.appwriteProjectId)
-        this.databases = new Databases(this.client)
-        this.bucket = new Storage(this.client)
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
 
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite DBService :: getPost() :: ", error);
+      return false;
     }
-
+  }
 }
