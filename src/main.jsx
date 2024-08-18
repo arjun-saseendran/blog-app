23fb9ad7ff446708cc0483abc8b5eebb217852a1
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-19 05:29:06 +0530

    wrap with provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
 const router = createBrowserRouter([]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
