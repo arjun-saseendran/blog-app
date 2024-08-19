f5c5c3b25696e8268175c1e16aef69f3940f5a9a
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-19 22:07:59 +0530

    add Home  page

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,42 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import DBService from '../appwrite/db'
-import Container from '../components/container/Container'
-import PostCard from '../components/PostCard'
+import React, { useEffect, useState } from "react";
+import DBService from "../appwrite/db";
+import Container from "../components/container/Container";
+import PostCard from "../components/PostCard";
 
 function AllPosts() {
+  const [posts, setPosts] = useState([]);
 
-const [posts, setPosts] = useState([])
-
-useEffect(() => {
-
-  DBService.getPosts([]).then((posts) => {
-    if(posts){
-      setPosts(posts.documents)
-    }
-  })
-
-
-}, [])
+  useEffect(() => {
+    DBService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
 
   return (
-    <div className='w-full py-8'>
+    <div className="w-full py-8">
       <Container>
-
-        <div className='flex flex-wrap'>
-{posts.map((post) => (
-<div className='p-2 w-1/4'>
-<PostCard {...post}/>
-
-</div>
-
-))}
-          
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
         </div>
-
       </Container>
-      
-      </div>
-  )
+    </div>
+  );
 }
 
-export default AllPosts
+export default AllPosts;
