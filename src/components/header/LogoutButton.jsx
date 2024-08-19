d3aa919148926bd0d730eb1eae596cc528b54e49
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-19 13:19:53 +0530

    add LogoutButton layout & functions

--- src/components/header/LogoutButton.jsx
+++ src/components/header/LogoutButton.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import authService from '../../appwrite/auth.js'
-import {logout} from '../../store/authSlice.js'
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth.js";
+import { logout } from "../../store/authSlice.js";
 
 function LogoutButton() {
-    const dispatch =useDispatch()
+  const dispatch = useDispatch();
 
-const logoutHandler = () => {
+  const logoutHandler = () => {
     authService.logout().then(() => {
-        dispatch(logout())
-    })
-}
+      dispatch(logout());
+    });
+  };
   return (
-    <button className='inline-block  px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={logoutHandler} >Logout</button>
-  )
+    <button
+      className="inline-block  px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
+  );
 }
 
-export default LogoutButton
+export default LogoutButton;
