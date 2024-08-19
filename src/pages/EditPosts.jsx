252f1193ca382bffbfff23bb3c21baaa77cfa60e
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-19 21:34:57 +0530

    add EditPosts page

--- src/pages/EditPosts.jsx
+++ src/pages/EditPosts.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import DBService from "../appwrite/db";
+import Container from "../components/container/Container";
+import PostForm from "../components/post-form/PostForm";
 
 function EditPosts() {
-  return (
-    <div>EditPosts</div>
-  )
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      DBService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        } else {
+          navigate("/");
+        }
+      });
+    }
+  }, [sulg, navigate]);
+
+  return <div className="py-6">
+<Container>
+  <PostForm post={post}/>
+</Container>
+
+
+  </div>;
 }
 
-export default EditPosts
+export default EditPosts;
