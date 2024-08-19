255bcbd60621fa49911d8f50997ff5eab53724f4
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-19 06:28:23 +0530

    add authentication logic

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Protected(children, authentication = true) {
-const authStatus = useSelector(state => state.auth.status)
-const navigate = useNavigate()
-const [loader, setLoader] = useState(false);
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
 
-  return (
-    <>{children}</>
-  )
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
+
+  return loader ? null : <>{children}</>;
 }
 
-export default Protected
+export default Protected;
--- src/main.jsx
+++ src/main.jsx
@@ -5,43 +5,74 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import Home from './pages/Home.jsx'
-import AuthLayout from './components/AuthLayout.jsx'
-import Signup from './pages/Signup.jsx'
-import AllPosts from './pages/AllPosts.jsx'
+import Home from "./pages/Home.jsx";
+import AuthLayout from "./components/AuthLayout.jsx";
+import Signup from "./pages/Signup.jsx";
+import AllPosts from "./pages/AllPosts.jsx";
+import AddPost from "./pages/AddPost.jsx";
+import EditPost from "./pages/EditPosts.jsx";
+import Post from "./pages/Post.jsx";
 
-const router = createBrowserRouter([{
-  path: '/', element: <App/>,
-  children: [{
-    path: '/', element: <Home/>
-  }, {
-    path: 'login', element:(
-      <AuthLayout authentication={false}>
-        <Login/>
-      </AuthLayout>
-)
-    
-  },
+const router = createBrowserRouter([
   {
-    path: 'signup', element:(
-      <AuthLayout authentication={false}>
-        <Signup/>
-      </AuthLayout>
-)
-    
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "all-posts",
+        element: (
+          <AuthLayout authentication>
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "add-post",
+        element: (
+          <AuthLayout authentication>
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "post/:slug",
+        element: (
+          <AuthLayout authentication>
+            <Post />
+          </AuthLayout>
+        ),
+      },
+    ],
   },
-  {
-    path: 'allposts', element:(
-      <AuthLayout authentication>
-        <AllPosts/>
-      </AuthLayout>
-)
-    
-  },
-
-
-]
-}]);
+]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
