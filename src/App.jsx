7e0d7102890bf9950c754201163077461a69aecf
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-20 04:04:56 +0530

    add App page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import authService from "./appwrite/auth";
 
 function App() {
-  return (
-    <>
-      <div>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [dispatch]);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
         <main>
           <Outlet />
         </main>
       </div>
-    </>
-  );
+      <div className="w-full block">
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
